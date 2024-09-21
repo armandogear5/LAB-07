e2c01f0a94f5b9bc107625bee29338a62989f8bd
Sergio Tacuri <seryitsarmin@gmail.com>
2024-09-21 17:33:37 -0400

    pregunta-1

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+export const List = () => {
+    return(
+        <ul>
+             <li>pan</li>
+             <li>queso</li>
+             <li>mantequilla</li>
+        </ul>
+       
+    )
+}
---
+++ src/components/WrapperList.jsx
@@ -0,0 +1,11 @@
+export const WapperList = props => {
+    const { children } = props
+    return(
+        <div>
+            <h2>Lista de compras</h2>
+        <div>
+            { children }
+        </div>
+        </div>
+    )
+}
